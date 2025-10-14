dd338d16fbb1e4915bbdd5c1d944e2931c4248f3
lukman.olarewaju <olarewajulukman8@gmail.com>
2025-10-14 03:53:20 -0700

    initial ccommit

---
+++ App.py
@@ -0,0 +1,81 @@
+# print("Hello Word")
+
+# if 5 > 2:
+#     print("Five is greater than two")
+# if 6 < 10:
+#  print("six is less ten")
+
+#  x = 5
+#  y = ("hello word")
+# #  print (x)
+# #  print (y)
+
+
+# x = 5
+# y = ("hello woeld")
+# print(x)
+# print(y)
+
+
+# x = 5
+# y = "john"
+# print (type(x))
+# print(type(y))
+
+# x = "awesome"
+
+# def myfunc():
+#     print("python is " + x)
+
+# myfunc ()    
+
+# x = "awesome"
+
+# def myfunc():
+#     x = "fantastic"
+#     print("python is " + x)
+    
+# # myfunc()
+# print("python is " + x)                        
+
+
+# course = "python programming"
+# print(len(course))
+# print(course[1])
+
+
+# fisrt = "olarewaju"
+# last = "olalekan"
+# full = fisrt + " " + last
+# print(full)
+
+# course = "python programming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+
+def my_area(l, b):
+    # # global x
+    # x = "Good morning everyone. Python language is sweet"
+    # print(x)
+    
+    area = l * b
+    # print(area)
+    return area
+# area()
+
+def volume():
+    h = 3
+    area = my_area(5, 8)
+    volume = area * h
+    return volume
+
+def basket_volume():
+    h = 3
+    area = my_area(4, 2)
+    volume = area * h
+    return volume
+print(volume())
+print(basket_volume())
+# print("python is " + x)
+
---
+++ python_tutorial.py
@@ -0,0 +1,26 @@
+# def legal_voters(age):
+#     if age < 18:
+#         print("You are underage and not allowed to vote")
+#     else:
+#         print("Please, kindly cast your vote. Thank you for civic duty execution")
+
+# legal_voters(17)
+
+
+# def area_of_rectangle(l, b):
+#     area = l * b
+#     return area
+
+
+# length = 5
+# breath = 5
+# print(area_of_rectangle(length, breath))
+
+def my_profile(firstname, lastname):
+    profile = f"Hello Everyone, my name is {firstname} {lastname} and I am happy to be here"
+    return profile
+
+firstname = "Mutmainnah"
+lastname = "Hammed"
+
+print(my_profile(firstname, lastname))
